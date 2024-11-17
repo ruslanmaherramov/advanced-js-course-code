f120bdbf0e6170817ffb1d2b63510411d320631c
ruslanmaherramov <rmaherramov@gmail.com>
2024-11-17 13:04:49 +0200

    add task from 8 section

---
+++ 08_ScopeAndClosures/closuresAndLoops.js
@@ -0,0 +1,14 @@
+// example of the problem with var
+// for (var i = 1; i < 6; i++) {
+//   (function (j) {
+//     setTimeout(function () {
+//       console.log(j);
+//     }, 1000 * j);
+//   })(i);
+// }
+
+for (let i = 1; i < 6; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 1000 * i);
+}
